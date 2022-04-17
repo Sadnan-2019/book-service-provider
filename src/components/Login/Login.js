1824aab856054fae835753939d4daf1523188bd4
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-04-18 04:26:21 +0600

    google login donee

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,69 +1,69 @@
-import { Button } from 'bootstrap';
-import React from 'react';
-import { Form } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
+// import { Button } from "bootstrap";
+// import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useSignInWithEmailAndPassword,useSignInWithGoogle } from "react-firebase-hooks/auth";
+// import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+import auth from "../../firebase.init";
 
 const Login = () => {
-
-          const [
-                    signInWithEmailAndPassword,
-                    user,
-                    loading,
-                    error,
-                  ] = useSignInWithEmailAndPassword(auth);
-          const navigate = useNavigate()
-
-          const navigateRegister=(event)=>{
-                    navigate("/register")
-
-          }
-
-          const handleLogin=(event)=>{
-                    event.preventDefault();
-                    const email = event.target.email.value;
-                    const password = event.target.password.value;
-                    signInWithEmailAndPassword(email,password)
-                    navigate("/")
+//   const [users, setGoogle] = useState({});
+  
+  const [signInWithGoogle, googleUser, loading, error] = useSignInWithGoogle(auth);
 
 
-                    // console.log("login")
-          }
-          return (
-                    <div className='w-50 mx-auto'>
+  const [signInWithEmailAndPassword, signInuser, signInloading, signInerror] =
+    useSignInWithEmailAndPassword(auth);
+  const navigate = useNavigate();
 
- <h3 className='text-center'>Please Login</h3>
- <Form onSubmit={handleLogin} >
+  const navigateRegister = (event) => {
+    navigate("/register");
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    signInWithEmailAndPassword(email, password);
+    navigate("/");
+
+    // console.log("login")
+  };
+
+  if(googleUser)
+  {
+    navigate('/')
+
+  }
+
+  return (
+    <div className="w-50 mx-auto">
+      <h3 className="text-center">Please Login</h3>
+      <Form onSubmit={handleLogin}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control
-            
-            type="email"
-            placeholder="Enter email"
-        
-            name='email'
-          />
-          
+          <Form.Control type="email" placeholder="Enter email" name="email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control
-           
             type="password"
             placeholder="Password"
-         
-            name='password'
+            name="password"
           />
         </Form.Group>
-        
-        <button className='btn btn-primary' type='submit'>Login</button>
+
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
       </Form>
+      <button className="btn btn-primary my-2" onClick={()=>{signInWithGoogle()}}>
+        SignWithGoogle
+      </button>
 
       <p className="text-center bg-dark text-light mt-2 rounded">
-       
         New to our book houee?{" "}
         <Link
           to="/register"
@@ -73,9 +73,8 @@ const Login = () => {
           Please Register
         </Link>
       </p>
-  
-                    </div>
-          );
+    </div>
+  );
 };
 
 export default Login;
